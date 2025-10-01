932f0b3fb00277e4725a6433bd7560b4b9b40af5
gamebos-collab <translovatogame@gmail.com>
2025-10-01 17:27:52 -0300

    TENTATIVA

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,7 +26,7 @@ export default function Home() {
     setErroLogin("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/login", {
+      const res = await fetch("https://portalbackend-i9xy.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
